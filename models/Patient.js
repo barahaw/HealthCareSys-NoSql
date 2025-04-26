a65dba75591fd417ee5f2d0a0a746575a2985fff
RawaaManassra <167885334+RawaaManassra@users.noreply.github.com>
2025-04-26 16:45:39 +0300

    Update Patient.js

--- models/Patient.js
+++ models/Patient.js
@@ -1,1 +1,35 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  medicalHistory: {
+    type: String,
+    required: true
+  },
+  doctorId: {
+    type: String,
+    required: true
+  },
+  region: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Patient', PatientSchema);
 
