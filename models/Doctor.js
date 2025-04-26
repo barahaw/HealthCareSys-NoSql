f736aa6bd866275ab3d2d6f7ad8a3eaf0d9c1aaf
RawaaManassra <167885334+RawaaManassra@users.noreply.github.com>
2025-04-26 16:52:32 +0300

    Create Doctor.js

---
+++ models/Doctor.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const DoctorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  specialty: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Doctor', DoctorSchema);
